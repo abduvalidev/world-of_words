dc6de85f5fb2f8d0ba99390e9aa0987bf6237f23
Abduvalidev <abduvalidev1@gmail.com>
2024-11-12 17:33:37 +0500

    set

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,27 +4,29 @@ import BackgrounLeft from "../assets/svg/background-left.svg";
 
 export default function Background() {
   return (
-    <div className="overflow-hidden top-0 w-full min-h-full h-full">
-      <img
-        src={BackgrounTop}
-        alt="background-top"
-        className="min-w-[109.5rem] object-center h-[50.9375rem] absolute top-0 -translate-x-1/2 left-1/2"
-      />
-      <img
-        src={BackgrounLeft}
-        alt="background-left"
-        className="hidden  min-h-[109.5rem] w-[50.9375rem] absolute top-1/2 -translate-y-1/2 left-0"
-      />
-      <img 
-        src={BackgrounLeft}
-        alt="background-left"
-        className="hidden min-h-[109.5rem] w-[50.9375rem] absolute top-1/2 -translate-y-1/2 right-0 rotate-180"
-      />
-      <img
-        src={BackgrounBottom}
-        alt="background-bottom"
-        className="min-w-[109.5rem] object-center h-[50.9375rem] absolute bottom-0 translate-y-10 -translate-x-1/2 left-1/2"
-      />
+    <div className="absolute z-0 size-full top-0 overflow-x-hidden">
+      <div className="relative overflow-hidden top-0 w-full min-h-full h-full">
+        <img
+          src={BackgrounTop}
+          alt="background-top"
+          className="min-w-[109.5rem] object-center h-[50.9375rem] absolute -translate-x-1/2 left-1/2"
+        />
+        <img
+          src={BackgrounLeft}
+          alt="background-left"
+          className="hidden  min-h-[109.5rem] w-[50.9375rem] absolute top-1/2 -translate-y-1/2 left-0"
+        />
+        <img
+          src={BackgrounLeft}
+          alt="background-left"
+          className="hidden min-h-[109.5rem] w-[50.9375rem] absolute top-1/2 -translate-y-1/2 right-0 rotate-180"
+        />
+        <img
+          src={BackgrounBottom}
+          alt="background-bottom"
+          className="min-w-[109.5rem] object-center h-[50.9375rem] absolute bottom-0 -translate-x-1/2 left-1/2"
+        />
+      </div>
     </div>
   );
 }
