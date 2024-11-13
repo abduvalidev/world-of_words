e1226d3abde4d99a903add846f710769eee3bac7
Abduvalidev <abduvalidev1@gmail.com>
2024-11-13 14:48:34 +0500

    first commit

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,3 +1,6 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
 import BackgrounTop from "../assets/svg/background-top.svg";
 import BackgrounBottom from "../assets/svg/background-bottom.svg";
 import BackgrounLeft from "../assets/svg/background-left.svg";
@@ -6,25 +9,29 @@ export default function Background() {
   return (
     <div className="absolute z-0 size-full top-0 overflow-x-hidden">
       <div className="relative overflow-hidden top-0 w-full min-h-full h-full">
-        <img
+        <LazyLoadImage
           src={BackgrounTop}
           alt="background-top"
           className="min-w-[109.5rem] object-center h-[50.9375rem] absolute -translate-x-1/2 left-1/2"
+          effect="blur" // Effektni o'rnatish
         />
-        <img
+        <LazyLoadImage
           src={BackgrounLeft}
           alt="background-left"
           className="hidden min-h-[109.5rem] w-[50.9375rem] absolute top-1/2 -translate-y-1/2 left-0"
+          effect="blur"
         />
-        <img
+        <LazyLoadImage
           src={BackgrounLeft}
           alt="background-left"
           className="hidden min-h-[109.5rem] w-[50.9375rem] absolute top-1/2 -translate-y-1/2 right-0 rotate-180"
+          effect="blur"
         />
-        <img
+        <LazyLoadImage
           src={BackgrounBottom}
           alt="background-bottom"
           className="min-w-[109.5rem] object-center h-[50.9375rem] absolute bottom-0 -translate-x-1/2 left-1/2"
+          effect="blur"
         />
       </div>
     </div>
